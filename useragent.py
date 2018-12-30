4c8b81e3fc68a517635654a1d8cb898d614e4f95
mikeyumingtao <34676307+mikeyumingtao@users.noreply.github.com>
2018-12-30 23:03:45 +0800

    Add files via upload

---
+++ useragent.py
@@ -0,0 +1,8 @@
+ua = [
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0.2) Gecko/20100101 Firefox/6.0.2",
+    "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.12",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0 Safari/537.36 OPR/15.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.57 Safari/537.17"
+]
